a1215475713ef7d07b7e7c70e7dbeb2853bdc802
royut <mohammadzareei36@gmail.com>
2026-04-16 19:17:21 -0700

    added block type function and enum

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,5 +1,38 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = 'paragraph'
+    HEADING = 'heading'
+    CODE = 'code'
+    QUOTE = 'quote'
+    UNORDERED_LIST = 'unordered_list'
+    ORDERED_LIST = 'ordered_list'
+
+
 def markdown_to_blocks(text):
     blocks = text.split('\n\n')
     blocks = list(map(lambda block: block.strip(), blocks))
     blocks = list(filter(lambda block: block != '', blocks))
     return blocks
+
+
+def block_to_block_type(block):
+    if block.startswith('# ') or block.startswith('## ') or block.startswith('### ') or block.startswith('#### ') or block.startswith('##### ') or block.startswith('###### '):
+        return BlockType.HEADING
+    if block.startswith('```\n') and block.endswith('\n```'):
+        return BlockType.CODE
+    lines = block.split('\n')
+    qoute_lines = list(filter(lambda line: line.startswith('>'), lines))
+    if len(qoute_lines) == len(lines):
+        return BlockType.QUOTE
+    unordered_lines = list(filter(lambda line: line.startswith('- '), lines))
+    if len(unordered_lines) == len(lines):
+        return BlockType.UNORDERED_LIST
+    ordered_flag = True
+    for i in range(len(lines)):
+        if not lines[i].startswith(f'{i+1}. '):
+            ordered_flag = False
+    if ordered_flag:
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, BlockType, block_to_block_type
 
 
 class TestFunction(unittest.TestCase):
@@ -46,6 +46,37 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+    
+    def test_block_to_block_type(self):
+        md = "## heading"
+        self.assertEqual(block_to_block_type(md), BlockType.HEADING)
+    
+    def test_block_to_block_type2(self):
+        md = """```
+code block
+```"""
+        self.assertEqual(block_to_block_type(md), BlockType.CODE)
+    
+    def test_block_to_block_type3(self):
+        md = """> quote
+> quote2"""
+        self.assertEqual(block_to_block_type(md), BlockType.QUOTE)
+    
+    def test_block_to_block_type4(self):
+        md = """- unordered 1
+- unordered 2"""
+        self.assertEqual(block_to_block_type(md), BlockType.UNORDERED_LIST)
+    
+    def test_block_to_block_type5(self):
+        md = """1. order1
+2. order2"""
+        self.assertEqual(block_to_block_type(md), BlockType.ORDERED_LIST)
+    
+    def test_block_to_block_type6(self):
+        md = """
+        asddsa
+        """
+        self.assertEqual(block_to_block_type(md), BlockType.PARAGRAPH)
 
 
 if __name__ == "__main__":
